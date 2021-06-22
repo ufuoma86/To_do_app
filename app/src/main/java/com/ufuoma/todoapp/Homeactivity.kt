5293654a3496e3bd55f480ee76c0cabc348d2b53
ufuoma86 <ufuomaavwata@gmail.com>
2021-06-22 17:29:34 +0100

    intent use

---
+++ app/src/main/java/com/ufuoma/todoapp/Homeactivity.kt
@@ -0,0 +1,19 @@
+package com.ufuoma.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Homeactivity : AppCompatActivity() {
+
+    private lateinit var usernameDisplay: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homeactivity)
+
+        usernameDisplay = findViewById(R.id.usernamedisplay)
+
+        usernameDisplay.text = intent.getStringExtra("name")
+    }
+}
--- app/src/main/java/com/ufuoma/todoapp/MainActivity.kt
+++ app/src/main/java/com/ufuoma/todoapp/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.ufuoma.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import java.net.PasswordAuthentication
+
+val validusername = "admin"
+val validpassword = "ädmin"
+private lateinit var usernameinput: EditText
+private lateinit var passwordinput: EditText
+private lateinit var submitbutton: EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        usernameinput = findViewById(R.id.UsernameInput)
+        passwordinput = findViewById(R.id.PasswordInput)
+        submitbutton = findViewById(R.id.submitbutton)
+
+        submitbutton.setOnClickListener {
+            val username = usernameinput.text.toString()
+            val password = passwordinput.text.toString()
+            login(username, password)
+        }
+
+    }
+
+    private fun login(username: String, password: String) {
+        if(isvalidcredentials(username, password)){
+            val intent: Intent =Intent(this,Homeactivity::class.java)
+            intent.putExtra("name",username)
+            startActivity(intent)
+
+        }else{
+            Toast.makeText(applicationContext,"something went wrong",Toast.LENGTH_SHORT).show()
+        }
+
+    }
+        private fun isvalidcredentials(username: String, password: String): Boolean{
+            if(username== validusername && password == validpassword)return true
+            return false
     }
 }
